e6a8f16c374ff73b1a7567ff946e752e502371b5
Catherine Morie <catherine.morie@wustl.edu>
2020-05-27 09:46:38 -0500

    create abstract Component class.

--- src/app.ts
+++ src/app.ts
@@ -113,6 +113,46 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   return adjDescriptor;
 }
 
+// Component Base Class (abstract - cannot be instanciated, only extended)
+abstract class Component<T extends HTMLElement, U extends HTMLElement> {
+  templateElement: HTMLTemplateElement;
+  hostElement: T;
+  element: U;
+
+  constructor(
+    templateId: string,
+    hostElementId: string,
+    insertAtStart: boolean,
+    newElementId?: string
+  ) {
+    this.templateElement = document.getElementById(
+      templateId
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById(hostElementId)! as T;
+
+    const importedHTMLContent = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedHTMLContent.firstElementChild as U;
+    if (newElementId) {
+      this.element.id = newElementId;
+    }
+    this.attach(insertAtStart);
+  }
+
+  private attach(insertAtBeginning: boolean) {
+    this.hostElement.insertAdjacentElement(
+      insertAtBeginning ? "afterbegin" : "beforeend",
+      this.element
+    );
+  }
+
+  // forces any class extending this to contain a configure and renderContent method to be determined at creation.
+  abstract configure(): void;
+  abstract renderContent(): void;
+}
+
 // ProjectList Class
 class ProjectList {
   templateElement: HTMLTemplateElement;
