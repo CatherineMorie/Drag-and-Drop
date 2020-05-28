d8915ca52c9d0d3ec735dadafab54595b92511e6
Catherine Morie <catherine.morie@wustl.edu>
2020-05-28 15:48:27 -0500

    create additional namespaces.

---
+++ src/components/base-component.ts
@@ -0,0 +1,44 @@
+namespace App {
+  // Component Base Class (abstract - cannot be instanciated, only extended)
+  export abstract class Component<
+    T extends HTMLElement,
+    U extends HTMLElement
+  > {
+    templateElement: HTMLTemplateElement;
+    hostElement: T;
+    element: U;
+
+    constructor(
+      templateId: string,
+      hostElementId: string,
+      insertAtStart: boolean,
+      newElementId?: string
+    ) {
+      this.templateElement = document.getElementById(
+        templateId
+      )! as HTMLTemplateElement;
+      this.hostElement = document.getElementById(hostElementId)! as T;
+
+      const importedHTMLContent = document.importNode(
+        this.templateElement.content,
+        true
+      );
+      this.element = importedHTMLContent.firstElementChild as U;
+      if (newElementId) {
+        this.element.id = newElementId;
+      }
+      this.attach(insertAtStart);
+    }
+
+    private attach(insertAtBeginning: boolean) {
+      this.hostElement.insertAdjacentElement(
+        insertAtBeginning ? "afterbegin" : "beforeend",
+        this.element
+      );
+    }
+
+    // forces any class extending this to contain a configure and renderContent method to be determined at creation.
+    abstract configure(): void;
+    abstract renderContent(): void;
+  }
+}
