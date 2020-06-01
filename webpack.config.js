4f56b7284aebbc03b3a94ed932fc41508bdd0af7
Catherine Morie <catherine.morie@wustl.edu>
2020-06-01 15:05:59 -0500

    base webpack setup and config

--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,19 @@ module.exports = {
   entry: './src/app.ts',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
-  devtools: "inline-source-map",
+  devtool: 'inline-source-map',
   module: {
-    rules: [{
-      test: /\.ts$/,
-      use: 'ts-loader',
-      exclude: /node_modules/
-    }]
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
   },
   resolve: {
-    extensions: ['.ts', '.js']
-  }
+    extensions: ['.ts', '.js'],
+  },
 };
